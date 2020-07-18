e1f5f5b4361a6a1a1d1dee3c589a3c51f96df673
Leafoliage <leafoliages@gmail.com>
2020-07-18 15:17:17 +0800

    [add] page hot

---
+++ pages/hot.js
@@ -0,0 +1,19 @@
+import Layout from '../component/layout'
+
+export default function Hot() {
+    return (
+        <Layout>
+            <div class="py-3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="display-5">熱門</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </Layout>
+    )
+}
