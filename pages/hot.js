d71330e0ca4457ace949a712209dbf7a7e43dafe
Leafoliage <leafoliages@gmail.com>
2020-07-18 15:33:47 +0800

    [modify] hot

--- pages/hot.js
+++ pages/hot.js
@@ -1,19 +1,27 @@
 import Layout from '../component/layout'
+import Post from '../component/article/post'
 
 export default function Hot() {
     return (
         <Layout>
-            <div class="py-3">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h1 class="display-5">熱門</h1>
+            <div className="py-3">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-5">熱門</h1>
                         </div>
                     </div>
                 </div>
             </div>
 
-
+            <main>
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </main>
         </Layout>
     )
 }
