5998eb960568886585946eb404e4775775a03e4a
Leafoliage <leafoliages@gmail.com>
2020-07-18 15:19:51 +0800

    [modify] layout

--- component/layout.js
+++ component/layout.js
@@ -21,15 +21,15 @@ export default function Layout({ children }) {
 
             <div>{children}</div>
 
-            <footer>
+            <footer className="fixed-bottom">
                 <div className="py-3">
                     <div className="container">
                         <div className="row">
                             <div className="col-md-12 text-center d-md-flex align-items-center">
-                                <i className="d-block fa fa-facebook-official text-muted fa-lg mx-4"></i><i
-                                    className="d-block fa fa-instagram text-muted fa-lg mx-4"></i><i
-                                        className="d-block fa fa-twitter text-muted fa-lg ml-0 mx-4"></i> <i
-                                            className="d-block fa fa-3x mx-auto text-primary fa-commenting"></i>
+                                <i className="d-block fa fa-facebook-official text-muted fa-lg mx-4" />
+                                <i className="d-block fa fa-instagram text-muted fa-lg mx-4" />
+                                <i className="d-block fa fa-twitter text-muted fa-lg ml-0 mx-4" />
+                                <i className="d-block fa fa-3x mx-auto text-primary fa-commenting" />
                                 <p className="mb-0 py-1">©2018 Pingendo All rights reserved</p>
                             </div>
                         </div>
@@ -37,13 +37,13 @@ export default function Layout({ children }) {
                 </div>
                 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
                     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                    crossorigin="anonymous"></script>
+                    crossorigin="anonymous" />
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
                     integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-                    crossorigin="anonymous"></script>
+                    crossorigin="anonymous" />
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
                     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-                    crossorigin="anonymous"></script>
+                    crossorigin="anonymous" />
             </footer>
         </div>
     )
