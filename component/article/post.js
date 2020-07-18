24ba7ffbec270e6a867bee42a346707c55053181
Leafoliage <leafoliages@gmail.com>
2020-07-18 16:16:14 +0800

    [modify] post

--- component/article/post.js
+++ component/article/post.js
@@ -5,9 +5,14 @@ export default function Post() {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="card">
-                            <div className="card-header">leafoliage&nbsp; &nbsp; &nbsp;2020.07.17-10:31</div>
+                            <div className="card-header">
+                                <div className="d-flex justify-content-between">
+                                    <div>leafoliage</div>
+                                    <div>2020.07.17-10:31</div>
+                                </div>
+                            </div>
                             <div className="card-body">
-                                <h4>阿好煩喔</h4>
+                                <h5>阿好煩喔</h5>
                                 <p className="mb-0">爛死了爛死了爛死了</p>
                                 <a className="btn py-0 px-1 pt-0 btn-link mt-1 mb-1" href="#">
                                     <i className="fa fa-heart-o fa-fw fa-1x py-1 text-warning"></i>
