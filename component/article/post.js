9ebbbca2289a60829ba179611cef5e72a1d56ff9
Leafoliage <leafoliages@gmail.com>
2020-07-18 15:33:40 +0800

    [add] post

---
+++ component/article/post.js
@@ -0,0 +1,22 @@
+export default function Post() {
+    return (
+        <div className="py-2">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">leafoliage&nbsp; &nbsp; &nbsp;2020.07.17-10:31</div>
+                            <div className="card-body">
+                                <h4>阿好煩喔</h4>
+                                <p className="mb-0">爛死了爛死了爛死了</p>
+                                <a className="btn py-0 px-1 pt-0 btn-link mt-1 mb-1" href="#">
+                                    <i className="fa fa-fw fa-1x py-1 fa-heart-o"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
