8f448d83b44ae93325b5881b78cf6662269208ab
Leafoliage <leafoliages@gmail.com>
2020-07-18 14:18:18 +0800

    [add] top

---
+++ component/top.js
@@ -0,0 +1,13 @@
+export default function Top() {
+    return (
+        <div class="text-center bg-primary py-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>協同中學學生意見平台 CMS-Forum</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
