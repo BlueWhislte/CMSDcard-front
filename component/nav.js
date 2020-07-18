e41494566d2404225c94bc50ec6a551fa4b251e0
Leafoliage <leafoliages@gmail.com>
2020-07-18 15:17:46 +0800

    [modify] navbar

--- component/nav.js
+++ component/nav.js
@@ -1,22 +1,46 @@
+import Link from 'next/link'
+
 export default function Nav() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            <div class="container"> <a class="navbar-brand" href="#">
-                <i class="fa d-inline fa-commenting fa-lg"></i>
-                <b></b>
-            </a> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse"
-                data-target="#navbar11">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div className="container">
+                <Link href="/">
+                    <a className="navbar-brand" href="#">
+                        <i className="fa d-inline fa-commenting fa-lg"></i>
+                    </a>
+                </Link>
+                <button className="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse"
+                    data-target="#navbar11">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbar11">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"> <a class="nav-link">熱門文章</a> </li>
-                        <li class="nav-item"> <a class="nav-link" href="#">先人文章</a> </li>
-                        <li class="nav-item"> <a class="nav-link" href="#">我要投稿</a> </li>
+                <div className="collapse navbar-collapse" id="navbar11">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <Link href="/hot">
+                                <a className="nav-link">熱門文章</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="">
+                                <a className="nav-link" href="#">先人文章</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="">
+                                <a className="nav-link" href="#">我要投稿</a>
+                            </Link>
+                        </li>
                     </ul>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"> <a class="nav-link" href="#">註冊</a> </li>
-                    </ul> <a class="btn btn-primary navbar-btn ml-md-2">登入</a>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link href="">
+                                <a className="nav-link" href="#">註冊</a>
+                            </Link>
+                        </li>
+                    </ul>
+                    <Link href="">
+                        <a className="btn btn-primary navbar-btn ml-md-2">登入</a>
+                    </Link>
                 </div>
             </div>
         </nav>
