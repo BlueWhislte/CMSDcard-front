b5b0d5a81bcd404183026962a8db616ea8733e67
Leafoliage <leafoliages@gmail.com>
2020-07-19 11:34:53 +0800

    [add] comment

---
+++ component/article/comment.js
@@ -0,0 +1,16 @@
+export default function Comment() {
+    return (
+        <div>
+            <h5 class="bg-light px-2 py-1 mb-0" >留言</h5>
+            <div class="list-group">
+                <div class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h6 class="mb-1">andytsai</h6>
+                        <small class="text-muted">2020.07.17-11:29</small>
+                    </div>
+                    <p class="mb-0">hemeheme</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- component/article/post.js
+++ component/article/post.js
@@ -1,3 +1,5 @@
+import Comment from './comment'
+
 export default function Post() {
     return (
         <div className="py-2">
@@ -15,8 +17,12 @@ export default function Post() {
                                 <h5>阿好煩喔</h5>
                                 <p className="mb-0">爛死了爛死了爛死了</p>
                                 <a className="btn py-0 px-1 pt-0 btn-link mt-1 mb-1" href="#">
-                                    <i className="fa fa-heart-o fa-fw fa-1x py-1 text-warning"></i>
+                                    <i className="fa fa-thumbs-o-up fa-fw fa-1x py-1 text-warning"></i>
                                 </a>
+                                <a className="btn py-0 px-1 pt-0 btn-link mt-1 mb-2" href="#">
+                                    <i className="fa fa-comment-o fa-fw fa-1x py-1 text-warning"></i>
+                                </a>
+                                <Comment />
                             </div>
                         </div>
                     </div>
