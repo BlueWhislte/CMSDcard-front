cf348180ddd4576bc0c03924c63d3f1dfe4acdeb
Leafoliage <leafoliages@gmail.com>
2020-07-19 10:41:56 +0800

    [modify] index background

--- pages/index.js
+++ pages/index.js
@@ -8,169 +8,30 @@ export default function Home() {
   return (
     <Layout>
       <main>
-        <div className="py-5 text-center text-white h-100 align-items-center d-flex" >
+        <div className="py-5 text-center text-white h-100 align-items-center d-flex" style={{backgroundImage: "linear-gradient(to bottom, rgba(0, 0, 0, .75), rgba(0, 0, 0, .75)), url(https://static.pingendo.com/cover-bubble-dark.svg)", backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "repeat"}}>
           <div className="container py-5">
             <div className="row">
               <div className="mx-auto col-lg-8 col-md-10">
-                <h1 className="display-3 text-dark mb-4">歡迎來到<br />協同學生意見平台</h1>
-                <p className="lead text-dark mb-5">這是一個專屬於協同人的論壇，請在這裡自由發表對學校、對課程等等的看法</p>
-                <Link href="">
-                  <a className="btn btn-lg btn-primary mx-1" style={{ backgroundColor: "#12bbad", borderColor: "#12bbad" }}>熱門文章</a>
+                <br />
+                <br />
+                <br />
+                <h1 className="display-3 mb-4">歡迎來到<br />協同學生意見平台</h1>
+                <p className="lead mb-5">這是一個專屬於協同人的論壇，請在這裡自由發表對學校、對課程等等的看法</p>
+                <Link href="/hot">
+                  <a className="btn btn-lg mx-1" style={{ borderColor: "#12bbad", color: "#12bbad" }}>熱門文章</a>
                 </Link>
                 <Link href="">
-                  <a className="btn btn-lg mx-1" style={{ borderColor: "#12bbad", color: "#12bbad" }}>我要註冊</a>
+                  <a className="btn btn-lg mx-1" style={{ backgroundColor: "#12bbad", borderColor: "#12bbad" }}>我要註冊</a>
                 </Link>
+                <br />
+                <br />
+                <br />
+                <br />
               </div>
             </div>
           </div>
         </div>
       </main>
-
-      {/* <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style> */}
-
-      {/* <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style> */}
     </Layout>
   )
 }
