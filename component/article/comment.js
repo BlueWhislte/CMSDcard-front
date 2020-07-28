88271000d7fdfd04082b2e89210e66ad73ff5535
Leafoliage <leafoliages@gmail.com>
2020-07-28 20:44:40 +0800

    [add] comment list item add key

--- component/article/comment.js
+++ component/article/comment.js
@@ -4,13 +4,13 @@ export default function Comment({ comments }) {
             <h5 className="bg-light px-2 py-1 mb-0">留言</h5>
             <div className="list-group">
                 {comments.map(comment => (
-                    <div className="list-group-item list-group-item-action flex-column align-items-start">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h6 className="mb-1">{comment.authorName}</h6>
-                        <small className="text-muted">{convertTime(comment.postTime)}</small>
+                    <div className="list-group-item list-group-item-action flex-column align-items-start" key={comment._id}>
+                        <div className="d-flex w-100 justify-content-between">
+                            <h6 className="mb-1">{comment.authorName}</h6>
+                            <small className="text-muted">{convertTime(comment.postTime)}</small>
+                        </div>
+                        <p className="mb-0">{comment.content}</p>
                     </div>
-                    <p className="mb-0">{comment.content}</p>
-                </div>
                 ))}
             </div>
         </div>
