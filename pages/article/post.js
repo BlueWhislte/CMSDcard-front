9a2250be2491a174a92eb2c82abcffc9186bcdd4
Leafoliage <leafoliages@gmail.com>
2020-07-28 21:10:37 +0800

    [add] post article function

--- pages/article/post.js
+++ pages/article/post.js
@@ -1,6 +1,21 @@
 import Layout from '../../component/layout'
 
 export default function Post() {
+
+    const postArticle = async () => {
+        return await fetch('http://localhost:6001/post', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                authorId: '5f0b0c0c9875a9f6b0ff6863',
+                title: document.getElementById('form-title').value,
+                content: document.getElementById('form-content').value
+            })
+        }).then(res => { res.json() })
+    }
+
     return (
         <Layout>
             <br />
@@ -14,15 +29,15 @@ export default function Post() {
                             <form className="text-left">
                                 <div className="form-group">
                                     <label htmlFor="form16">標題</label>
-                                    <input type="text" className="form-control" id="form16" placeholder="你的主題" />
+                                    <input type="text" className="form-control" id="form-title" placeholder="你的主題" />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="form17">內文</label>
                                     <div className="form-group">
-                                        <textarea className="form-control" id="form30" rows="6" placeholder="你想說的話..."></textarea>
+                                        <textarea className="form-control" id="form-content" rows="6" placeholder="你想說的話..."></textarea>
                                     </div>
                                 </div>
-                                <button type="submit" className="btn" style={{background:"#12bbad", color:"#FFFFFF"}}>潑文</button>
+                                <button type="submit" className="btn" style={{background:"#12bbad", color:"#FFFFFF"}} onClick={postArticle}>潑文</button>
                             </form>
                         </div>
                     </div>
