63d02e581dd223ae88087d38b6c800812ddc5067
Leafoliage <leafoliages@gmail.com>
2020-07-30 18:09:33 +0800

    [modify] date string

--- functions/utils.js
+++ functions/utils.js
@@ -1,4 +1,12 @@
 export function convertTime(iso) {
     let date = new Date(iso)
-    return date.toLocaleString()
+
+    let month = date.getMonth() < 10 ? '0'+date.getMonth() : date.getMonth()
+    let day = date.getDay() < 10 ? '0'+date.getDay() : date.getDay()
+    let hours = date.getHours() < 10 ? '0'+date.getHours() : date.getHours()
+    let minutes = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()
+
+    let dateString = date.getFullYear() + '-' + month + '-' + day
+    let timeString = hours + ':' + minutes
+    return dateString + ' ' + timeString
 }
