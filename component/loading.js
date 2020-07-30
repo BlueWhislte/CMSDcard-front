19ace5ca9e8a43f76d978beaf5578fb996703826
Leafoliage <leafoliages@gmail.com>
2020-07-30 18:14:49 +0800

    [modify] className

--- component/loading.js
+++ component/loading.js
@@ -1,16 +1,16 @@
 export default function Loading() {
     return (
-        <div class="py-5" >
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-1">
+        <div className="py-5" >
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-1">
                         <br />
                         <br />
                         <br />
                         <br />
                         <br />
                         <br />
-                        <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+                        <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
                         <br />
                         <br />
                         <br />
