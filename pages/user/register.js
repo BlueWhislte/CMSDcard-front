75c4959b3c2e11939f58b8ff11583d0cc24a101e
Leafoliage <leafoliages@gmail.com>
2020-08-04 10:36:46 +0800

    [modify] page register

--- pages/user/register.js
+++ pages/user/register.js
@@ -3,15 +3,14 @@ import Layout from '../../component/layout'
 export default function Register() {
 
     const postRegister = async () => {
-        return await fetch('http://localhost:6001/user', {
+        return await fetch('http://localhost:6001/user/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 name: document.getElementById('form-name').value,
-                email: document.getElementById('form-email').value,
-                password: document.getElementById('form-pass').value
+                email: document.getElementById('form-email').value
             })
         })
             .then(res => { res.json() })
@@ -29,14 +28,18 @@ export default function Register() {
                             <form className="text-left">
                                 <div className="form-group">
                                     <label htmlFor="form16">使用者名稱</label>
-                                    <small className="ml-2 text-muted">ex: mark_the_handsome、fanny.weng、協同金城武、真難溶於水...</small>
+                                    <small className="ml-2 text-muted"> ex: mark_the_handsome、fanny.weng、協同金城武、真難溶於水...</small>
                                     <input type="text" className="form-control" id="form-name" placeholder="mark_the_handsome" />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="form18">電子郵件</label>
                                     <input type="email" className="form-control" id="form-email" placeholder="mark@gmail.com" />
                                 </div>
-                                <div className="form-row">
+                                <div className="form-group">
+                                    <label htmlFor="form20">密碼</label>
+                                    <p>將由系統生成暫時密碼，並傳送至您的電子郵件信箱。</p>
+                                </div>
+                                {/* <div className="form-row">
                                     <div className="form-group col-md-6">
                                         <label htmlFor="form19">密碼</label>
                                         <input type="password" className="form-control" id="form-pass" placeholder="••••" />
@@ -45,7 +48,7 @@ export default function Register() {
                                         <label htmlFor="form20">確認密碼</label>
                                         <input type="password" className="form-control" id="form-confirmPass" placeholder="••••" />
                                     </div>
-                                </div>
+                                </div> */}
                                 <button type="submit" className="btn mt-1" style={{ background:"#12bbad", color:"#ffffff" }} onClick={postRegister}>註冊</button>
                             </form>
                         </div>
