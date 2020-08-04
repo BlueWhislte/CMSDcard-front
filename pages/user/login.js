8a47fdbd2e92a61ac9b4b4893e7c11516110f4c0
Leafoliage <leafoliages@gmail.com>
2020-08-04 10:42:22 +0800

    [modify] page login

--- pages/user/login.js
+++ pages/user/login.js
@@ -1,6 +1,23 @@
 import Layout from '../../component/layout'
 
 export default function Login() {
+
+    const postLogin = async () => {
+        return await fetch('http://localhost:6001/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer`
+            },
+            body: JSON.stringify({
+                email: document.getElementById('form-email').value,
+                password: document.getElementById('form-pass').value
+            })
+        })
+            .then(res => { res.json() })
+            .catch(err => { console.log(err) })
+    }
+
     return (
         <Layout>
             <div className="py-5 text-center" style={{ backgroundImage: "url('https://static.pingendo.com/cover-bubble-dark.svg')", backgroundSize: "cover" }} >
@@ -13,15 +30,15 @@ export default function Login() {
                             <h1 className="mb-4">登入</h1>
                             <form>
                                 <div className="form-group">
-                                    <input type="email" className="form-control" placeholder="電子郵件" id="form9" />
+                                    <input type="email" className="form-control" placeholder="電子郵件" id="form-email" />
                                 </div>
                                 <div className="form-group mb-3">
-                                    <input type="password" className="form-control" placeholder="密碼" id="form10" />
+                                    <input type="password" className="form-control" placeholder="密碼" id="form-pass" />
                                     <small className="form-text text-muted text-right mt-2">
                                         <a href="#">顯示密碼</a>
                                     </small>
                                 </div>
-                                <button type="submit" className="btn" style={{background:"#12bbad", color:"#ffffff"}}>登入</button>
+                                <button type="submit" className="btn" onClick={postLogin} style={{ background: "#12bbad", color: "#ffffff" }}>登入</button>
                             </form>
                         </div>
                     </div>
