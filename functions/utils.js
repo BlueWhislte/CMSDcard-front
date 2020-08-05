5cada859a68f6e4933ac348513c4b5027a89fdef
Leafoliage <leafoliages@gmail.com>
2020-08-05 16:11:41 +0800

    [modify] convertTime

--- functions/utils.js
+++ functions/utils.js
@@ -1,8 +1,10 @@
 export function convertTime(iso) {
     let date = new Date(iso)
+    let getMonth = date.getMonth() + 1
+    let getDay = date.getDay() + 2
 
-    let month = date.getMonth() < 10 ? '0'+date.getMonth() : date.getMonth()
-    let day = date.getDay() < 10 ? '0'+date.getDay() : date.getDay()
+    let month = getMonth < 10 ? '0'+getMonth : getMonth
+    let day = getDay < 10 ? '0'+getDay : getDay
     let hours = date.getHours() < 10 ? '0'+date.getHours() : date.getHours()
     let minutes = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes()
 
