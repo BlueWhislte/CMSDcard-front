b43c7d4173f3efd066ad8ac1454b5bbdc2fbe1da
Leafoliage <leafoliages@gmail.com>
2020-08-06 08:52:05 +0800

    [fix] convert time

--- functions/utils.js
+++ functions/utils.js
@@ -49,7 +49,6 @@ export function convertDateOnly(iso) {
 
     let month = parseMonth(date)
     let day = parseDay(date)
-    date.getMi
 
     return date.getFullYear() + '-' + month + '-' + day
 }
