2a1c50cdef700ea0c0d7e0bb7466e5bd1b540580
Leafoliage <leafoliages@gmail.com>
2020-08-16 22:12:44 +0800

    [modify] 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,12 @@
 import Head from 'next/head'
+import Layout from '../component/layout'
 
 export default function NotFound() {
     return (
-        <div className="d-flex align-items-center" style={{background: "#000000", position: "fixed", width:"100%", height: "100%"}}>
-            <Head>
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-                    crossOrigin="anonymous" />
-            </Head>
-            <img src="404.jpg" className="mx-auto" style={{display:"block", width:"30%", verticalAlign:"center"}} />
-        </div>
+        <Layout>
+            <div className="d-flex align-items-center" style={{ background: "#000000", width: "100%", height: "100%", padding: "4rem" }}>
+                <img src="404.jpg" className="mx-auto" style={{ display: "block", width: "30%", verticalAlign: "center" }} />
+            </div>
+        </Layout>
     )
 }
