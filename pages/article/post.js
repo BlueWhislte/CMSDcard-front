fa529bd757aff9d038266bd50eb2c717642e7516
Leafoliage <leafoliages@gmail.com>
2020-08-18 22:23:13 +0800

    [modify] page post article

--- pages/article/post.js
+++ pages/article/post.js
@@ -34,7 +34,7 @@ export default function Post() {
                     <div className="row">
                         <div className="mx-auto col-lg-6 col-10">
                             <h1>投稿</h1>
-                            <p className="mb-3">你的每個意見都是寶貴的意見!</p>
+                            <p className="mb-3">你的每個想法都是寶貴的意見!</p>
                             <form className="text-left">
                                 <div className="form-group">
                                     <label htmlFor="form16">標題</label>
@@ -43,7 +43,7 @@ export default function Post() {
                                 <div className="form-group">
                                     <label htmlFor="form17">內文</label>
                                     <div className="form-group">
-                                        <textarea className="form-control" id="form-content" rows="6" placeholder="你想說的話..."></textarea>
+                                        <textarea className="form-control" id="form-content" rows="10" placeholder="你想說的話..."></textarea>
                                     </div>
                                 </div>
                                 <button type="button" className="btn" style={{ background: "#12bbad", color: "#FFFFFF" }} onClick={postArticle}>潑文</button>
@@ -52,7 +52,6 @@ export default function Post() {
                     </div>
                 </div>
             </div>
-            <br />
             <br />
         </Layout>
     )
