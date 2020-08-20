8a47f1c42821af9d7d5acfc82b29b5dc84ce24ad
Leafoliage <leafoliages@gmail.com>
2020-08-20 17:20:40 +0800

    [modify] page about us

--- pages/about-us.js
+++ pages/about-us.js
@@ -29,11 +29,20 @@ export default function AboutUs() {
                     </div>
                     <div className="row mb-4 mt-5 d-flex justify-content-center">
                         <div className="p-0 order-2 order-lg-1 col-lg-2">
-                            <img className="img-fluid d-block" src="lemon.png" />
+                            <img className="img-fluid d-block" src="email.png" />
                         </div>
                         <div className="d-flex flex-column justify-content-center p-3 col-lg-7 order-1 order-lg-2">
+                            <h3>聯絡我們</h3>
+                            <p className="lead mb-0">Email: cmsforum.official@gmail.com</p>
+                        </div>
+                    </div>
+                    <div className="row mt-2 mb-5 d-flex justify-content-center">
+                        <div className="d-flex flex-column justify-content-center p-3 col-lg-7">
                             <h3>網站作者</h3>
-                            <p className="lead mb-0">鹿介，一位平凡的協同中學高X生。喜歡打code，因而把整個暑假都奉獻給這個網站了。平常會彈吉他、看Youtube當消遣，遊戲只玩Minecraft。整天只知道打code的下場就是書都沒有讀，成績不知道會發生什麼事情QQ。</p>
+                            <p className="lead mb-0">鹿介，一位協同中學高X生。喜歡打code，因而把整個暑假都奉獻給這個網站了。平常會彈吉他、看Youtube當消遣，遊戲只玩Minecraft。整天只知道打code的下場就是書都沒有讀，成績不知道會發生什麼事情QQ。</p>
+                        </div>
+                        <div className="p-0 col-lg-2">
+                            <img className="img-fluid d-block" src="lemon.png" />
                         </div>
                     </div>
                 </div>
