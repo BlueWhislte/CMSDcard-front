cd42101d26ef73c828d0c806963e040637868f57
Leafoliage <leafoliages@gmail.com>
2020-08-26 13:04:56 +0800

    [modify] set input autoComplete to off

--- pages/user/login.js
+++ pages/user/login.js
@@ -41,10 +41,10 @@ export default function Login() {
                             <h1 className="mb-4">登入</h1>
                             <form>
                                 <div className="form-group">
-                                    <input type="email" className="form-control" placeholder="電子郵件" id="form-email" />
+                                    <input type="email" className="form-control" placeholder="電子郵件" id="form-email" autoComplete="off" />
                                 </div>
                                 <div className="form-group mb-3">
-                                    <input type="password" className="form-control" placeholder="密碼" id="form-pass" />
+                                    <input type="password" className="form-control" placeholder="密碼" id="form-pass" autoComplete="off" />
                                     {/* <small className="form-text text-muted text-right mt-2">
                                         <a href="#">顯示密碼</a>
                                     </small> */}
