8f2d3b25a1537b100121d2a2d74ad56f61e716b9
Leafoliage <leafoliages@gmail.com>
2020-09-20 21:06:09 +0800

    [modify] top responsing

--- component/top.js
+++ component/top.js
@@ -4,8 +4,8 @@ export default function Top() {
     return (
         <div className="text-center py-4" style={{ backgroundColor: themeColor }}>
             <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
+                <div className="row justify-content-center">
+                    <div className="col-md-12 col-sm-12 col-7">
                         <h1>協同學生論壇 CMS Forum</h1>
                     </div>
                 </div>
