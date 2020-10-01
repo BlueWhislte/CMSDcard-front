40a408b12ba7437231a9f5a1829b5552a047480b
Leafoliage <leafoliages@gmail.com>
2020-10-01 08:17:25 +0800

    [modify] page article style

--- component/article/comment.js
+++ component/article/comment.js
@@ -45,17 +45,15 @@ export default function Comment({ comments, postId }) {
                 </button>
             </div>
             <small className="ml-1 text-muted">按 Ctrl + Enter 傳送</small>
-            <br />
-            <div className="list-group">
+            
+            <div className="list-group mt-3">
                 {comments.map(comment => (
                     <div className="list-group-item list-group-item-action flex-column align-items-start" key={comment._id}>
                         <div className="d-flex w-100 justify-content-between">
                             <h6 className="mb-1">{comment.authorName}</h6>
                             <small className="text-muted">{convertBriefTime(comment.postTime)}</small>
                         </div>
-                        <div className="mb-0">
-                            <Linkify tagName="p" style={{ whiteSpace: "pre-line" }}>{comment.content}</Linkify>
-                        </div>
+                        <Linkify className="my-1" tagName="p" style={{ whiteSpace: "pre-line" }}>{comment.content}</Linkify>
                     </div>
                 ))}
             </div>
