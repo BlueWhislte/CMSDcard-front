127323e67453ea588d4a9241b9d21e38d1cc907e
Leafoliage <leafoliages@gmail.com>
2020-10-04 16:11:41 +0800

    [modify] top

--- component/top.js
+++ component/top.js
@@ -2,14 +2,6 @@ import { themeColor } from '../functions/utils'
 
 export default function Top() {
     return (
-        <div className="text-center py-4" style={{ backgroundColor: themeColor }}>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-12 col-sm-12 col-7">
-                        <h1>協同學生論壇 CMS Forum</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div className="py-5" style={{ backgroundImage: "url(/img/top.png)", backgroundPosition:"center"}} samesite="None" />
     )
 }
