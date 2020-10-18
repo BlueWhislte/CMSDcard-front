a27e94497203d42b8f1eb85363041c8afb10fff2
Leafoliage <leafoliages@gmail.com>
2020-10-18 17:22:03 +0800

    [modify] use state to preserve comment [add] click on username to responnd

--- component/article/comment.js
+++ component/article/comment.js
@@ -1,20 +1,20 @@
 import { convertBriefTime, themeColor } from '../../functions/utils'
 import Linkify from 'linkifyjs/react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Comment({ comments, postId, onComment }) {
-    useEffect(() => {
-        document.getElementById('form-comment').addEventListener('keypress', (e) => {
-            console.log(e)
-            if (e.ctrlKey && e.code === 'Enter') {
-                postComment()
-                document.getElementById('form-comment').value = ''
-            }
-        })
-    })
+    const [comment, setComment] = useState('')
+
+    // useEffect(() => {
+    //     document.getElementById('form-comment').addEventListener('keypress', (e) => {
+    //         if (e.ctrlKey && e.code === 'Enter') {
+    //             postComment()
+    //         }
+    //     })
+    // })
 
     const postComment = async () => {
-        if (!document.getElementById('form-comment').value) return
+        if (!comment) return
 
         return await fetch(`${process.env.NEXT_PUBLIC_API_URL}/comment/${postId}`, {
             method: 'POST',
@@ -24,13 +24,13 @@ export default function Comment({ comments, postId, onComment }) {
             },
             body: JSON.stringify({
                 authorId: '5f28c3d328666f21bca62f7e',
-                content: document.getElementById('form-comment').value
+                content: comment
             })
         }).then(async res => {
             if (!res.ok) window.alert('Sorry!  Σ(･口･)   ' + await res.text())
         })
             .then(() => {
-                document.getElementById('form-comment').value = ''
+                setComment('')
                 onComment()
             })
     }
@@ -39,7 +39,7 @@ export default function Comment({ comments, postId, onComment }) {
         <div>
             <div className="row">
                 <div className="mt-1 col-xl-11 col-lg-11 col-md-10 col-sm-10 col-10">
-                    <textarea rows="1" id="form-comment" type="text" className="form-control" placeholder="留言" autoComplete="off" />
+                    <textarea rows="1" id="form-comment" type="text" className="form-control" value={comment} placeholder="留言" autoComplete="off" onChange={(e) => { setComment(e.target.value) }} />
                 </div>
                 <button type="button" className="btn mt-1" style={{ backgroundColor: themeColor, color: "#ffffff" }} onClick={postComment}>
                     <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chat-right-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -48,13 +48,13 @@ export default function Comment({ comments, postId, onComment }) {
                     </svg>
                 </button>
             </div>
-            <small className="ml-1 text-muted">按 Ctrl + Enter 傳送</small>
-            
+            {/* <small className="ml-1 text-muted">按 Ctrl + Enter 傳送</small> */}
+
             <div className="list-group mt-3">
                 {comments.map(comment => (
                     <div className="list-group-item list-group-item-action flex-column align-items-start" key={comment._id}>
                         <div className="d-flex w-100 justify-content-between">
-                            <h6 className="mb-1">{comment.authorName}</h6>
+                            <h6 className="mb-1" onClick={() => { setComment(comment.authorName + ' ') }}>{comment.authorName}</h6>
                             <small className="text-muted">{convertBriefTime(comment.postTime)}</small>
                         </div>
                         <Linkify className="my-1" tagName="p" style={{ whiteSpace: "pre-line" }}>{comment.content}</Linkify>
