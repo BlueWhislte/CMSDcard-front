e24831f06219850ee613bac1c964a760afbd9e53
Leafoliage <leafoliages@gmail.com>
2020-12-12 16:38:07 +0800

    [modify] search

--- pages/article/search.js
+++ pages/article/search.js
@@ -1,24 +1,26 @@
 import Layout from '../../component/layout'
 import Post from '../../component/article/post'
-import Loading from '../../component/loading'
+import PagingButtons from '../../component/block/pagingButtons'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { themeColor } from '../../functions/utils'
 
 export default function Search() {
     const router = useRouter()
     const [data, setData] = useState()
+    const [regex, setRegex] = useState('')
 
     useEffect(() => {
         if (!localStorage.getItem('auth')) router.push('/user/login')
 
-        document.getElementById('form-regex').addEventListener('keyup', (e)=>{
+        document.getElementById('form-regex').addEventListener('keyup', (e) => {
             if (e.key === 'Enter') search()
         })
     })
 
-    const search = async () => {
-        if (!document.getElementById('form-regex').value) return
+    const search = async (page) => {
+        if (!regex) return
+
+        console.log('first:', regex)
 
         const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post/search`, {
             method: 'PUT',
@@ -27,10 +29,13 @@ export default function Search() {
                 'Authorization': `Bearer ${localStorage.getItem('auth')}`
             },
             body: JSON.stringify({
-                regex: document.getElementById('form-regex').value
+                regex: regex,
+                page: page || 1
             })
         })
-        if (data.status == 401 || data.status == 403)  {
+
+        console.log('second:', regex)
+        if (data.status == 401 || data.status == 403) {
             router.push('/user/login')
             router.reload()
         }
@@ -47,8 +52,9 @@ export default function Search() {
                             <h1 className="display-5 mt-3">搜尋文章</h1>
                             <hr />
                             <div className="row justify-content-center">
-                                <input type="text" className="form-control col-md-8 col-8 mr-1" id="form-regex" placeholder="關鍵字" />
-                                <button type="button" className="btn" onClick={search}>
+                                <input type="text" className="form-control col-md-8 col-8 mr-1" id="form-regex" placeholder="關鍵字"
+                                    onChange={(e) => setRegex(e.target.value)} />
+                                <button type="button" className="btn" onClick={()=>search()}>
                                     <i className="fa fa-search mr-1" aria-hidden="true" />
                                     搜尋
                             </button>
@@ -60,7 +66,7 @@ export default function Search() {
 
             <main>
                 {data && data.length != 0 ?
-                    data.map(post => (
+                    data.docs.map(post => (
                         <Post post={post} key={post._id} />
                     )) : (
                         <div className="container text-center my-5 py-5">
@@ -68,6 +74,11 @@ export default function Search() {
                         </div>
                     )
                 }
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <PagingButtons totalPage={data && data.totalPages || 0} currPage={data && data.page || 0} changePage={search} />
+                    </div>
+                </div>
             </main>
         </Layout>
     )
